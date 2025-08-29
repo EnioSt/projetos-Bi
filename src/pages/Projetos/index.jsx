9ace2f31864cdda93806bcecad488b6ca7b1846e
Enio dos Santos Junior <enio.junior@grupochq.com.br>
2025-08-29 16:47:16 -0300

    Atualização da pagina de projetos

--- src/pages/Projetos/index.jsx
+++ src/pages/Projetos/index.jsx
@@ -6,29 +6,25 @@ const Projetos = () => {
       <Inner>
         <div>
           <h2>Tempo médio de atendimento</h2>
-          <a href="https://app.powerbi.com/view?r=eyJrIjoiZmI4MTQ4MGItOWQ3Yi00MDg3LTg2OGEtYTNmNTUzZTNlN2UyIiwidCI6IjllM2MzMzRjLWY2MmEtNDA4NC04ZTYyLTMyMDZhNDBlMWY2MyJ9">
-            <iframe
-              title="TMA"
-              width="600"
-              height="373.5"
-              src="https://app.powerbi.com/view?r=eyJrIjoiZmI4MTQ4MGItOWQ3Yi00MDg3LTg2OGEtYTNmNTUzZTNlN2UyIiwidCI6IjllM2MzMzRjLWY2MmEtNDA4NC04ZTYyLTMyMDZhNDBlMWY2MyJ9"
-              frameborder="0"
-              allowFullScreen="true"
-            ></iframe>
-          </a>
+          <iframe
+            title="TMA"
+            width="600"
+            height="373.5"
+            src="https://app.powerbi.com/view?r=eyJrIjoiZmI4MTQ4MGItOWQ3Yi00MDg3LTg2OGEtYTNmNTUzZTNlN2UyIiwidCI6IjllM2MzMzRjLWY2MmEtNDA4NC04ZTYyLTMyMDZhNDBlMWY2MyJ9"
+            frameborder="0"
+            allowFullScreen="true"
+          ></iframe>
         </div>
         <div>
           <h2>Pesquisa de satisfação</h2>
-          <a href="https://app.powerbi.com/view?r=eyJrIjoiMmFkNWQ3MmQtZmQ2Ni00ZjY5LThmZGMtN2JlOGZiYzM0NGJhIiwidCI6IjllM2MzMzRjLWY2MmEtNDA4NC04ZTYyLTMyMDZhNDBlMWY2MyJ9">
-            <iframe
-              title="pesquisa_satisfacao"
-              width="600"
-              height="373.5"
-              src="https://app.powerbi.com/view?r=eyJrIjoiMmFkNWQ3MmQtZmQ2Ni00ZjY5LThmZGMtN2JlOGZiYzM0NGJhIiwidCI6IjllM2MzMzRjLWY2MmEtNDA4NC04ZTYyLTMyMDZhNDBlMWY2MyJ9"
-              frameborder="0"
-              allowFullScreen="true"
-            ></iframe>
-          </a>
+          <iframe
+            title="pesquisa_satisfacao"
+            width="600"
+            height="373.5"
+            src="https://app.powerbi.com/view?r=eyJrIjoiMmFkNWQ3MmQtZmQ2Ni00ZjY5LThmZGMtN2JlOGZiYzM0NGJhIiwidCI6IjllM2MzMzRjLWY2MmEtNDA4NC04ZTYyLTMyMDZhNDBlMWY2MyJ9"
+            frameborder="0"
+            allowFullScreen="true"
+          ></iframe>
         </div>
       </Inner>
     </Container>
